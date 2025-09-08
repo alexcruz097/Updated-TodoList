55648751ac98d0ff675f45a5cac1797344c640a5
alexcruz097 <alexc017@yahoo.com>
2025-09-07 23:29:07 -0700

    changed from manual appending 1 by 1 element to appending the whole array with map and added an onload

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ const removeBTN = document.querySelector(".deleteBTN");
 const todoText = document.querySelector("#todo-form");
 const todoDateInput = document.querySelector("#todo-date");
 const todoForm = document.querySelector(".todo-form");
-const showFormBTN = document.querySelector(".show-form-btn");
-const todoList = [];
+const addFormBTN = document.querySelector(".add-form-btn");
+const editBTN = document.querySelector(".editBTN");
+const editButtonForm = document.querySelector(".edit-btn");
+const todoList = [
+  { todo: "Go to work", dueDate: "02/15/2025", completed: false, id: "12324" },
+  {
+    todo: "Go to the park",
+    dueDate: "5/25/2025",
+    completed: false,
+    id: "7465745",
+  },
+  {
+    todo: "Go to program get a job",
+    dueDate: "4/29/2026",
+    completed: false,
+    id: "34564",
+  },
+];
 
 // create random color
 const randomColor = () => {
@@ -16,9 +32,19 @@ const randomColor = () => {
   return `rgb(${r}, ${g} ,${b})`;
 };
 
-const showForm = () => {
+// dynamicaly two form
+const addForm = () => {
   todoForm.classList.toggle("show-form");
   todoForm.classList.toggle("hide-form");
+  addBTN.style = "display: block";
+  editButtonForm.style = "display:none;";
+};
+
+const editForm = () => {
+  todoForm.classList.toggle("show-form");
+  todoForm.classList.toggle("hide-form");
+  addBTN.style = "display:none;";
+  editButtonForm.style = "display:block;";
 };
 
 const createBTN = (className, innerText) => {
@@ -28,12 +54,10 @@ const createBTN = (className, innerText) => {
   return editBTN;
 };
 // create todo Template
-const todoTemplate = (todoText, date , id) => {
+const todoTemplate = (todoText, date, id) => {
   // create li
   let Li = document.createElement("li");
   Li.classList.add("todo");
-  // random backcolor for each item
-  Li.style.backgroundColor = randomColor();
   // create edit/delete btn and p for date
   let editBTN = createBTN("editBTN", "Edit");
   let deleteBTN = createBTN("deleteBTN", "Delete");
@@ -48,102 +72,69 @@ const todoTemplate = (todoText, date , id) => {
   Li.appendChild(deleteBTN);
   return Li;
 };
+//generate todo
+const generateTodo = () => {
+  listContainer.innerHTML = "";
+  todoList.map((todo) => {
+    listContainer.appendChild(todoTemplate(todo.todo, todo.dueDate, todo.id));
+  });
+};
+window.onload = generateTodo;
+
 // create item and push to arr
 const createTodo = (e) => {
   e.preventDefault();
-  // get textt
+  let randomID = Math.floor(
+    Math.random() * Math.floor(Math.random() * Date.now())
+  );
   // get date and format it
   const dateObject = new Date(todoDateInput.value);
   let dueDate = dateObject.toLocaleDateString();
   // push new todo to the array
-  todoList.push({
-    text: todoText.value,
+  let newTodo = {
+    todo: todoText.value,
     dueDate: dueDate,
     completed: false,
-    id: todoList.length,
-  });
-  console.log(todoList)
-  // append new item into the pate
-  let listItem = todoTemplate(todoText.value, dueDate , todoList.id);
-  listContainer.appendChild(listItem);
+    id: randomID,
+  };
+  todoList.push(newTodo);
+  // after pushing generate todo
+  generateTodo();
   // clear form
   todoText.value = "";
   todoDateInput.valueAsDate = null;
 
   // hide form
-  showForm();
+  addForm();
 };
-const updateTodo = ()=>{
 
-}
-const removeTodo = (e) => {
+const editTodo = () => {
   alert("click");
 };
-showFormBTN.addEventListener("click", showForm);
+
+addFormBTN.addEventListener("click", addForm);
 addBTN.addEventListener("click", createTodo);
+editButtonForm.addEventListener("click", (e) => {
+  e.preventDefault();
+  editTodo();
+  
+});
 
 listContainer.addEventListener("click", (e) => {
   // delete Item
   if (e.target.classList.contains("deleteBTN")) {
+    // remove item from the  arr
+    todoList.map((todo, index) => {
+      if (todo.id == e.target.parentNode.id) {
+        todoList.splice(index, 1);
+      }
+    });
+    // remove item from the node
     e.target.parentNode.remove();
   }
-// edit item
-if(e.target.classList.contains("editBTN")){
-showForm();
-e.target.parentNode.innerHTML  = `${todoText}
-
-`
-
-
-}
-
+  // edit item
+  if (e.target.classList.contains("editBTN")) {
+    editForm();
+    console.log(e.target.parentNode.id)
+  }
 });
-//append to the page
-
-// const createItem = (e) => {
-//   e.preventDefault();
-//   let promptItem = prompt("Add item list");
-//   // create edit button
-//   let editBTN = createBTN("editBTN", "Edit");
-//   //create delete button
-//   let deleteBTN = createBTN("deleteBTN", "Delete");
-//   // create element
-//   let newItem = document.createElement("li");
-//   // add value to new item
-//   newItem.textContent = promptItem;
-//   // class
-//   newItem.classList.add("todo");
-//   // add random background color
-//   newItem.style.backgroundColor = randomColor();
-//   // add edit button into the list item
-//   newItem.appendChild(editBTN);
-//   // add delete btn
-//   newItem.appendChild(deleteBTN);
-//   // add newitem to container
-//   listContainer.appendChild(newItem);
-//   // clear the text
-// };
-// const deleteItem = (event) => {
-//   // if li is click then crossed it out
-//   event.target.parentNode.remove();
-// };
-
-// const editItem = (event) => {
-//   let updatedItem = prompt("Update your todo item");
-//   // add item to the list
-//   event.target.parentNode.innerHTML = `${updatedItem} <button class="editBTN">Edit</button> <button class="deleteBTN">Delete</button>`;
-// };
-
-// // add item event listener
-// addBTN.addEventListener("click", createItem);
-
-// // delete item listener
-// listContainer.addEventListener("click", function (event) {
-//   if (event.target.classList.contains("deleteBTN")) {
-//     deleteItem(event);
-//   }
-//   // add event to edit button
-//   if (event.target.classList.contains("editBTN")) {
-//     editItem(event);
-//   }
-// });
